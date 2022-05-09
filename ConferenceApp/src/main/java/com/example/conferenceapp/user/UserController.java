dfaba95ff259b593b412014f0c5f9a81633de092
p-wojt <patryk.wojtiuk9@gmail.com>
2022-05-09 07:31:30 +0200

    add user controller

---
+++ ConferenceApp/src/main/java/com/example/conferenceapp/user/UserController.java
@@ -0,0 +1,17 @@
+package com.example.conferenceapp.user;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(final UserService userService) {
+        this.userService = userService;
+    }
+}
